1af4249d445472a7bcdc9633cb11a6f7515e1f0c
Shermyl <katherinehuamancondori@gmail.com>
2025-08-04 02:05:09 -0500

    ya esta el local :3

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,20 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [react()], // <-- Solo esto
-    resolve: {
-    alias: {
-        '@': resolve(__dirname, './src')
+  plugins: [react()],
+  server: {
+    port: 5173,
+    open: '/index.html' // Abre automáticamente
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'public/index.html'),
+        biotienda: resolve(__dirname, 'public/biotienda.html'),
+        menu: resolve(__dirname, 'public/menu.html'),
+        admin: resolve(__dirname, 'public/admin/index.html')
+      }
     }
-}
+  }
 });
